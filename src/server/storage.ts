3b9ea9d5f5f9c937c503cb3c2eafcb94184c65b6
hhhjin <kkp7125@gmail.com>
2023-02-08 20:28:04 +0900

    fix r2 env variables

--- src/server/storage.ts
+++ src/server/storage.ts
@@ -5,7 +5,7 @@ export const R2 = new S3Client({
   region: "auto",
   endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
-    accessKeyId: env.R2_CLIENT_ID,
-    secretAccessKey: env.R2_CLIENT_SECRET,
+    accessKeyId: env.R2_ACCESS_KEY_ID,
+    secretAccessKey: env.R2_SECRET_ACCESS_KEY,
   },
 });
